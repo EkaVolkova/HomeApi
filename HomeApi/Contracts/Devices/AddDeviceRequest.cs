9d986ec4b4e3ce3ca138882d26bccce1e2f977d3
Ekaterina Volkova <koweevaea@gmail.com>
2024-09-15 20:22:56 +0500

    Добавила обязательный ввод всех полей при запросе добавления устройства

--- HomeApi/Contracts/Devices/AddDeviceRequest.cs
+++ HomeApi/Contracts/Devices/AddDeviceRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,12 +11,46 @@ namespace HomeApi.Contracts.Devices
     /// </summary>
     public class AddDeviceRequest
     {
+        /// <summary>
+        /// Название устройства (обязательное поле)
+        /// </summary>
+        [Required]
         public string Name { get; set; }
+
+        /// <summary>
+        /// Производитель устройства (обязательное поле)
+        /// </summary>
+        [Required]
         public string Manufacturer { get; set; }
+
+        /// <summary>
+        /// Модель устройства (обязательное поле)
+        /// </summary>
+        [Required]
         public string Model { get; set; }
+
+        /// <summary>
+        /// Серийный номер устройства (обязательное поле)
+        /// </summary>
+        [Required]
         public string SerialNumber { get; set; }
+
+        /// <summary>
+        /// Напряжение питания устройства (обязательное поле)
+        /// </summary>
+        [Required]
         public int CurrentVolts { get; set; }
+
+        /// <summary>
+        /// Работает ли на газу (обязательное поле)
+        /// </summary>
+        [Required]
         public bool GasUsage { get; set; }
+
+        /// <summary>
+        /// Расположение устройства (обязательное поле)
+        /// </summary>
+        [Required]
         public string Location { get; set; }
     }
 }
