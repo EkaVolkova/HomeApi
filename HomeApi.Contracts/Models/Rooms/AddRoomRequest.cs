33001ce9a89eec76f0cd8e3fbb0111147049c48c
Ekaterina Volkova <koweevaea@gmail.com>
2024-10-06 21:28:14 +0500

    Добавила Модель для запроса добавления комнаты, поправила валидацию запроса добавления устройства

---
+++ HomeApi.Contracts/Models/Rooms/AddRoomRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Models.Rooms
+{
+    public class AddRoomRequest
+    {
+
+        /// <summary>
+        /// Название комнаты
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Плозадь комнаты
+        /// </summary>
+        public int Area { get; set; }
+
+        /// <summary>
+        /// Подключение газа к комнате
+        /// </summary>
+        public bool GasConnected { get; set; }
+
+        /// <summary>
+        /// Напряжение питания в комнате
+        /// </summary>
+        public int Voltage { get; set; }
+    }
+}
--- HomeApi.Contracts/Validation/Devices/AddDeviceRequestValidator.cs
+++ HomeApi.Contracts/Validation/Devices/AddDeviceRequestValidator.cs
@@ -10,38 +10,21 @@ namespace HomeApi.Contracts.Validation.Devices
 {
     public class AddDeviceRequestValidator : AbstractValidator<AddDeviceRequest>
     {
-        string[] _validLocations;
 
         public AddDeviceRequestValidator()
         {
             // Сохраним список допустимых вариантов размещения устройств
-            _validLocations = new[]
-            {
-               "Kitchen",
-               "Bathroom",
-               "Livingroom",
-               "Toilet"
-            };
             RuleFor(x => x.Name).NotEmpty();
             RuleFor(x => x.Manufacturer).NotEmpty();
             RuleFor(x => x.Model).NotEmpty();
             RuleFor(x => x.SerialNumber).NotEmpty();
             RuleFor(x => x.CurrentVolts)
                 .NotEmpty()
-                .InclusiveBetween(120,200)
+                .InclusiveBetween(120,220)
                 .WithMessage("Поддерживается напряжение питания от 120 до 220 В");
-            RuleFor(x => x.GasUsage).NotEmpty();
-            RuleFor(x => x.Location).NotEmpty().Must(BeSupported);
+            RuleFor(x => x.Location).NotEmpty();
         }
 
-        /// <summary>
-        ///  Метод кастомной валидации для свойства location
-        /// </summary>
-        private bool BeSupported(string location)
-        {
-            // Проверим, содержится ли значение в списке допустимых
-            return _validLocations.Any(e => e == location);
-        }
 
     }
 }
