ff089546c38754f23423beec1a396f7708768c9a
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-10-07 13:43:13 +0500

    Добавила в репозиторий для комнат обновление комнаты в БД в соответствие с запросом и получение объекта комнаты из БД по Id

--- HomeApi.Data/Repos/IRoomRepository.cs
+++ HomeApi.Data/Repos/IRoomRepository.cs
@@ -1,4 +1,6 @@
 ﻿using HomeApi.Data.Models.Home;
+using HomeApi.Data.Queries;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -20,10 +22,26 @@ namespace HomeApi.Data.Repos
         Task<Room> GetRoomByName(string name);
 
         /// <summary>
+        /// Получить объект класса Room из БД по id асинхронно
+        /// </summary>
+        /// <param name="id">Идентификатор комнаты</param>
+        /// <returns>Комната с идентификатором, соответствующим id</returns>
+        Task<Room> GetRoomById(Guid id);
+
+
+        /// <summary>
         /// Получить список имеющихся комнат асинхронно
         /// </summary>
         /// <returns>Список комнат</returns>
         Task<List<Room>> GetRooms();
+
+        /// <summary>
+        /// Обновить комнату
+        /// </summary>
+        /// <param name="room">Модель комнаты</param>
+        /// <param name="query">Запр</param>
+        /// <returns></returns>
+        Task UpdateRoom(Room room, UpdateRoomQuery query);
 
 
     }
--- HomeApi.Data/Repos/RoomRepository.cs
+++ HomeApi.Data/Repos/RoomRepository.cs
@@ -1,5 +1,6 @@
 ﻿using HomeApi.Data.Models;
 using HomeApi.Data.Models.Home;
+using HomeApi.Data.Queries;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -30,6 +31,16 @@ namespace HomeApi.Data.Repos
         }
 
         /// <summary>
+        /// Получить объект класса Room из БД по id асинхронно
+        /// </summary>
+        /// <param name="id">Идентификатор комнаты</param>
+        /// <returns>Комната с идентификатором, соответствующим id</returns>
+        public async Task<Room> GetRoomById(Guid id)
+        {
+            return await _context.Rooms.FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        /// <summary>
         /// Получить объект класса Room из БД по названию асинхронно
         /// </summary>
         /// <param name="name">Название комнаты</param>
@@ -48,6 +59,37 @@ namespace HomeApi.Data.Repos
             return await _context.Rooms
                 .Include(d => d.Devices)
                 .ToListAsync();
+        }
+
+        /// <summary>
+        /// Обновить комнату
+        /// </summary>
+        /// <param name="room">Модель комнаты</param>
+        /// <param name="query">Запр</param>
+        /// <returns></returns>
+        public async Task UpdateRoom(Room room, UpdateRoomQuery query)
+        {
+
+            if (!string.IsNullOrEmpty(query.NewName))
+                room.Name = query.NewName;
+
+            if (query.NewArea != null)
+                room.Area = (int)query.NewArea;
+
+            if (query.NewGasConnected != null)
+                room.GasConnected = (bool)query.NewGasConnected;
+
+            if (query.NewVoltage != null)
+                room.Voltage = (int)query.NewVoltage;
+
+            // Обновление 
+            var entry = _context.Entry(room);
+            if (entry.State == EntityState.Detached)
+                _context.Rooms.Update(room);
+
+            // Сохранение изменений
+            await _context.SaveChangesAsync();
+        
         }
     }
 }
