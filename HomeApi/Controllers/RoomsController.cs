70ca5f61aad41ae6ce800074d7673c6d7832a93d
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-10-07 13:45:53 +0500

    Добавила функцию обновления комнаты в RoomsController

--- HomeApi/Controllers/RoomsController.cs
+++ HomeApi/Controllers/RoomsController.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using HomeApi.Contracts.Models.Rooms;
 using HomeApi.Data.Models.Home;
+using HomeApi.Data.Queries;
 using HomeApi.Data.Repos;
 using HomeApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System;
 using System.Threading.Tasks;
 
 namespace HomeApi.Controllers
@@ -48,6 +50,42 @@ namespace HomeApi.Controllers
             await _repository.AddRoom(room);
 
             return StatusCode(200, $"Комната {request.Name} добавлено!");
+        }
+
+        /// <summary>
+        /// Обновить устройство
+        /// </summary>
+        /// <param name="id">Идентификатор устройства</param>
+        /// <param name="request">Модель запроса обновления</param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("Edit{id}")]
+        public async Task<IActionResult> EditAsyc(
+            [FromRoute] Guid id,
+            [FromBody]
+            EditRoomRequest request
+            )
+        {
+
+            //Проверяем, есть ли комната с данным id в БД
+            var room = await _repository.GetRoomById(id);
+            if (room == null)
+                return StatusCode(400, $"Комната c Id = {id} отсутствует!");
+
+            //Проверяем, что комнаты с таким названием еще нет в БД
+            var roomByName = await _repository.GetRoomByName(request.NewName);
+            if (roomByName != null && roomByName.Id != id)
+                return StatusCode(400, $"Комната с названием {request.NewName} уже существует");
+
+            //Преобразуем EditRoomRequest в UpdateRoomQuery при помощи AutoMapper
+            var query = _mapper.Map<EditRoomRequest, UpdateRoomQuery>(request);
+
+            //Обновляем данные в БД
+            await _repository.UpdateRoom(room, query);
+
+            //Возвращаем код успешного завершения операции
+            return StatusCode(200, $"Комната c id = {id} изменено!");
+
         }
     }
 }
