c07d72f0e85a6082a3c0cf41d5cd570de43670f0
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-10-07 13:44:25 +0500

    Добавила запрос на изменение комнаты в Contracts

---
+++ HomeApi.Contracts/Models/Rooms/EditRoomRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Models.Rooms
+{
+    public class EditRoomRequest
+    {
+        /// <summary>
+        /// Название комнаты
+        /// </summary>
+        public string NewName { get; set; }
+
+        /// <summary>
+        /// Плозадь комнаты
+        /// </summary>
+        public int? NewArea { get; set; }
+
+        /// <summary>
+        /// Подключение газа к комнате
+        /// </summary>
+        public bool? NewGasConnected { get; set; }
+
+        /// <summary>
+        /// Напряжение питания в комнате
+        /// </summary>
+        public int? NewVoltage { get; set; }
+    }
+}
