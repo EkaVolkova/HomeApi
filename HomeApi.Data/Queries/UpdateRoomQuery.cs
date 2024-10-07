b5d044e3832362bd483c15fab202293cefe217ee
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-10-07 13:41:41 +0500

    Добавила модель запроса для обновления комнаты в БД

---
+++ HomeApi.Data/Queries/UpdateRoomQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Data.Queries
+{
+    public class UpdateRoomQuery
+    {
+
+        /// <summary>
+        /// Название комнаты
+        /// </summary>
+        public string NewName { get; }
+
+        /// <summary>
+        /// Плозадь комнаты
+        /// </summary>
+        public int? NewArea { get; }
+
+        /// <summary>
+        /// Подключение газа к комнате
+        /// </summary>
+        public bool? NewGasConnected { get; }
+
+        /// <summary>
+        /// Напряжение питания в комнате
+        /// </summary>
+        public int? NewVoltage { get; }
+
+        public UpdateRoomQuery(string newName = null,
+                               int? newArea = null,
+                               bool? newGasConnected = null,
+                               int? newVoltage = null)
+        {
+            NewName = newName;
+            NewArea = newArea;
+            NewGasConnected = newGasConnected;
+            NewVoltage = newVoltage;
+        }
+    }
+}
